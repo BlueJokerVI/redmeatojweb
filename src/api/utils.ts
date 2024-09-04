d10274b8f4db71ab74c9e63d36afb6861e4bc81f
BlueCat <15757048847@163.com>
2024-09-04 16:35:14 +0800

    1. 项目初始化 2. 项目精简，实现登入注册

---
+++ src/api/utils.ts
@@ -0,0 +1,1 @@
+export const baseUrlApi = (url: string) => `http://localhost:8101/api/${url}`;
---
+++ src/views/register/index.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="select-none">
+    <img :src="bg" class="wave" />
+    <div class="flex-c absolute right-5 top-3">
+      <!-- 主题 -->
+      <el-switch
+        v-model="dataTheme"
+        inline-prompt
+        :active-icon="dayIcon"
+        :inactive-icon="darkIcon"
+        @change="dataThemeChange"
+      />
+    </div>
+    <div class="login-container">
+      <div class="img">
+        <component :is="toRaw(illustration)" />
+      </div>
+      <div class="login-box">
+        <div class="login-form">
+          <avatar class="avatar" />
+          <Motion>
+            <h2 class="outline-none">{{ title }}</h2>
+          </Motion>
+
+          <el-form
+            ref="ruleFormRef"
+            :model="ruleForm"
+            :rules="loginRules"
+            size="large"
+          >
+            <Motion :delay="100">
+              <el-form-item
+                :rules="[
+                  {
+                    required: true,
+                    message: '请输入账号',
+                    trigger: 'blur'
+                  }
+                ]"
+                prop="account"
+              >
+                <el-input
+                  v-model="ruleForm.account"
+                  clearable
+                  placeholder="账号"
+                  :prefix-icon="useRenderIcon(User)"
+                />
+              </el-form-item>
+            </Motion>
+
+            <Motion :delay="150">
+              <el-form-item prop="password">
+                <el-input
+                  v-model="ruleForm.password"
+                  clearable
+                  show-password
+                  placeholder="密码"
+                  :prefix-icon="useRenderIcon(Lock)"
+                />
+              </el-form-item>
+            </Motion>
+
+            <Motion :delay="150">
+              <el-form-item prop="password">
+                <el-input
+                  v-model="ruleForm.repeatPassword"
+                  clearable
+                  show-password
+                  placeholder="再次输入密码"
+                  :prefix-icon="useRenderIcon(Lock)"
+                />
+              </el-form-item>
+            </Motion>
+
+            <Motion :delay="250">
+              <el-button
+                class="w-full mt-4"
+                size="default"
+                type="default"
+                :loading="loading"
+                @click="onRegister"
+              >
+                注册
+              </el-button>
+            </Motion>
+
+            <Motion :delay="250">
+              <el-button
+                class="w-full mt-2"
+                size="default"
+                type="primary"
+                :loading="loading"
+                @click="goLogin"
+              >
+                登录
+              </el-button>
+            </Motion>
+          </el-form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Motion from "./utils/motion";
+import { useRouter } from "vue-router";
+import { message } from "@/utils/message";
+import { loginRules } from "./utils/rule";
+import { useNav } from "@/layout/hooks/useNav";
+import type { FormInstance } from "element-plus";
+import { useLayout } from "@/layout/hooks/useLayout";
+import { useUserStoreHook } from "@/store/modules/user";
+import { initRouter, getTopMenu } from "@/router/utils";
+import { bg, avatar, illustration } from "./utils/static";
+import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+import { ref, reactive, toRaw, onMounted, onBeforeUnmount } from "vue";
+import { useDataThemeChange } from "@/layout/hooks/useDataThemeChange";
+
+import dayIcon from "@/assets/svg/day.svg?component";
+import darkIcon from "@/assets/svg/dark.svg?component";
+import Lock from "@iconify-icons/ri/lock-fill";
+import User from "@iconify-icons/ri/user-3-fill";
+import { userRegister } from "@/api/user";
+import { error } from "console";
+defineOptions({
+  name: "Register"
+});
+const router = useRouter();
+const loading = ref(false);
+
+const { initStorage } = useLayout();
+initStorage();
+
+const { dataTheme, overallStyle, dataThemeChange } = useDataThemeChange();
+dataThemeChange(overallStyle.value);
+const { title } = useNav();
+
+const ruleForm = reactive({
+  account: "",
+  password: "",
+  repeatPassword: ""
+});
+
+const onRegister = () => {
+  userRegister(ruleForm)
+    .then(res => {
+      if (res.code === 0) {
+        message(`注册成功`, { type: "success" });
+      } else {
+        message(`注册失败，${res.message}`, { type: "error" });
+      }
+    })
+    .catch(error => {
+      message(`注册失败，${error}`, { type: "error" });
+    });
+};
+const goLogin = () => {
+  router.push("/login");
+};
+</script>
+
+<style scoped>
+@import url("@/style/login.css");
+</style>
+
+<style lang="scss" scoped>
+:deep(.el-input-group__append, .el-input-group__prepend) {
+  padding: 0;
+}
+</style>
