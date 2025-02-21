6cfdfdfa7102cfba855ee7bc2fd24ef31da7a97f
RedMeat <1545860023@qq.com>
2025-02-21 14:51:40 +0800

    feat: 适配HTTPS证书，修改URL

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,3 +1,2 @@
 // export const baseUrlApi = (url: string) => `http://127.0.0.1:8101/api/${url}`;
-export const baseUrlApi = (url: string) =>
-  `http://www.oj.redmeat.site:10504/api/${url}`;
+export const baseUrlApi = (url: string) => `/api/${url}`;
